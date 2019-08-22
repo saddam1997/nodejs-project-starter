064e356fdbb3644fcf49ad94f4ee3bf42fd62c9b
saddam1997 <saddam.husen1997@gmail.com>
2019-08-22 18:09:27 +0530

    First commit

---
+++ db/index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose'),
+    dbconf = require('../config/db');
+
+let dbString = 'mongodb://' + dbconf.dbcredentials.user;
+dbString = dbString + ':' + dbconf.dbcredentials.password;
+dbString = dbString + '@' + dbconf.dbcredentials.address;
+dbString = dbString + ':' + dbconf.dbcredentials.port;
+dbString = dbString + '/' + dbconf.dbcredentials.database;
+
+mongoose.connect(dbString, {
+    useCreateIndex: true,
+    useNewUrlParser: true
+});
+mongoose.Promise = global.Promise;
+module.exports = {
+    User: require('./models/user')
+};
